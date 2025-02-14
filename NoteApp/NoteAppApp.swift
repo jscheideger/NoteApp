d9571bdf5839ba7eb558386c34b19276206d4f8d
Jesten Scheideger <jesten.scheideger@okstate.edu>
2025-02-14 14:24:09 -0600

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Jesten Scheideger on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
